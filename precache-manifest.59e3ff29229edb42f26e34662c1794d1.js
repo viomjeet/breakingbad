2a44e49bc43ec930b5d9b9d4410f907513e3714c
vikas kumar <viomjeet@gmail.com>
2020-07-04 21:55:57 +0530

    Updates

---
+++ precache-manifest.59e3ff29229edb42f26e34662c1794d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb452a0db824ce6b98ee73f83b875d8",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "47cf5062da7ef2a088bd",
+    "url": "/breakingbad/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "db2c667c045eca3604d3",
+    "url": "/breakingbad/static/css/main.8f3317df.chunk.css"
+  },
+  {
+    "revision": "47cf5062da7ef2a088bd",
+    "url": "/breakingbad/static/js/2.d01dfc7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/breakingbad/static/js/2.d01dfc7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2c667c045eca3604d3",
+    "url": "/breakingbad/static/js/main.d5d1b34e.chunk.js"
+  },
+  {
+    "revision": "ea2d3b5d3b339cb26b3a",
+    "url": "/breakingbad/static/js/runtime-main.e09d8a2d.js"
+  }
+]);
